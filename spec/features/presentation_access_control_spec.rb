1d8ae62cfd9cffa4120160a27506de09c664f89e
Andrew Cole <aocole@gmail.com>
2015-04-29 11:53:12 -0700

    Add specs for deleting presentations

--- spec/features/presentation_access_control_spec.rb
+++ spec/features/presentation_access_control_spec.rb
@@ -10,7 +10,7 @@ describe "slideshow access control" do
       log_in_as user
       user_slideshow = FactoryGirl.create(:slideshow, user: user)
       other_slideshow = FactoryGirl.create(:slideshow, user: other_user)
-      visit(presentations_path)
+      visit_expect(presentations_path)
       expect(find('table')).to have_content user_slideshow.name
       expect(find('table')).not_to have_content other_slideshow.name
     end
@@ -18,17 +18,26 @@ describe "slideshow access control" do
     it "should allow editing own slideshows" do
       log_in_as user
       user_slideshow = FactoryGirl.create(:slideshow, user: user)
-      visit(edit_slideshow_path(user_slideshow))
-      expect(current_path).to eq(edit_slideshow_path(user_slideshow))
+      visit_expect(edit_slideshow_path(user_slideshow))
       expect(find_field('Name').value).to eq user_slideshow.name
       fill_in('Name', with: Faker::Commerce.product_name)
       click_button 'Save'
     end
     
+    it "should allow deleting own foldershows" do
+      log_in_as user
+      FactoryGirl.create(:slideshow, user: user)
+      visit_expect(presentations_path)
+      expect(page).to have_xpath(".//table/tbody/tr", :count => 1)
+      click_link_or_button('Delete')
+      expect(current_path).to eq presentations_path
+      expect(page).not_to have_xpath(".//table/tbody/tr")
+    end
+    
     it "should allow creating slideshows" do
       log_in_as user
       theme = FactoryGirl.create(:theme)
-      visit(new_slideshow_path)
+      visit_expect(new_slideshow_path)
       expect(page).to have_select('Theme', options: [theme.name])
       name = Faker::Commerce.product_name
       fill_in('Name', with: name)
@@ -57,7 +66,7 @@ describe "foldershow access control" do
       log_in_as user
       user_foldershow = FactoryGirl.create(:foldershow, user: user)
       other_foldershow = FactoryGirl.create(:foldershow, user: other_user)
-      visit(presentations_path)
+      visit_expect(presentations_path)
       expect(find('table')).to have_content user_foldershow.name
       expect(find('table')).not_to have_content other_foldershow.name
     end
@@ -65,16 +74,25 @@ describe "foldershow access control" do
     it "should allow editing own foldershows" do
       log_in_as user
       user_foldershow = FactoryGirl.create(:foldershow, user: user)
-      visit(edit_foldershow_path(user_foldershow))
-      expect(current_path).to eq(edit_foldershow_path(user_foldershow))
+      visit_expect(edit_foldershow_path(user_foldershow))
       expect(find_field('Name').value).to eq user_foldershow.name
       fill_in('Name', with: Faker::Commerce.product_name)
       click_button 'Save'
     end
     
+    it "should allow deleting own foldershows" do
+      log_in_as user
+      FactoryGirl.create(:foldershow, user: user)
+      visit_expect(presentations_path)
+      expect(page).to have_xpath(".//table/tbody/tr", :count => 1)
+      click_link_or_button('Delete')
+      expect(current_path).to eq presentations_path
+      expect(page).not_to have_xpath(".//table/tbody/tr")
+    end
+    
     it "should allow creating foldershows" do
       log_in_as user
-      visit(new_foldershow_path)
+      visit_expect(new_foldershow_path)
       name = Faker::Commerce.product_name
       fill_in('Name', with: name)
       click_button 'Save'
