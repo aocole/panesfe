c041e4391d8c533d4884ceb16ac220263f6d9350
Andrew Cole <aocole@gmail.com>
2015-05-05 17:09:56 -0700

    Second pass on video thumbnailer

--- lib/carrierwave/video_thumbnailer.rb
+++ lib/carrierwave/video_thumbnailer.rb
@@ -11,11 +11,11 @@ module CarrierWave
     def video_thumb(width, height)
       FFMPEG.logger = Rails.logger
       FFMPEG.ffmpeg_binary = 'avconv'
-      Tempfile.create("video_thumbnailer_") do |f|
-        FileUtils.move(current_path, f.path)
-        file = ::FFMPEG::Movie.new(f.path)
+      Tempfile.create(['video_thumbnailer_', '.png']) do |tempfile|
+        file = ::FFMPEG::Movie.new(current_path)
         # file.transcode(current_path, "-ss 00:00:05 -an -r 1 -vframes 1 -s #{width}x#{height}")
-        file.screenshot(current_path, { seek_time: 5, resolution: "#{width}x#{height}" }, preserve_aspect_ratio: :width)
+        file.screenshot(tempfile.path, { seek_time: 5, resolution: "#{width}x#{height}" }, preserve_aspect_ratio: :width)
+        FileUtils.copy(tempfile.path, current_path)
       end
     end
 
